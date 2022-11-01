73e1a694e381dc6d2c3e4bb9346da5657ea2c4ad
kiim-wong <guili_008@foxmail.com>
2022-11-01 21:10:59 +0800

    Create meihao.js

---
+++ ycdz/meihao.js
@@ -0,0 +1,25 @@
+/*******************************
+美豪酒店
+*******************************
+[rewrite_local]
+^https:\/\/api\.wx\.gcihotel\.net\/api\/shop url script-response-body https://raw.githubusercontent.com/kiim-wong/Shell/master/service/meihao.js
+[mitm] 
+hostname = api.wx.gcihotel.net
+*******************************/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const mumu = '/flashGoodsDetail';
+
+
+if (url.indexOf(mumu) != -1) {
+    obj.retVal.flashGoods.dayStartTime = "2022-11-01 10:00:00";
+    obj.retVal.flashGoods.startTime = "2022-11-01 10:00:00";
+
+	body = JSON.stringify(obj);
+}
+
+
+$done({body});
